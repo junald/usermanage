28232b8f70870dacbbaf432a51dd7db5b3c61f41
junald lavador <junaldcl@yahoo.com>
2017-03-17 14:28:43 +0800

    usermanage

---
+++ src/main/java/virtx3/Server.java
@@ -0,0 +1,27 @@
+package virtx3;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+
+/*
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class Server extends AbstractVerticle {
+
+	// Convenience method so you can run it in your IDE
+	public static void main(String[] args) {
+		Runner.runExample(Server.class);
+	}
+
+	@Override
+	public void start() throws Exception {
+
+		Router router = Router.router(vertx);
+
+		router.route().handler(routingContext -> {
+			routingContext.response().putHeader("content-type", "text/html").end("Hello World!1");
+		});
+
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+	}
+}
